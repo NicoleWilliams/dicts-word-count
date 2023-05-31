74104c1c2133e5525b0c29932127660c2586472c
Nicole Williams <nicole.boesenberg@gmail.com>
2023-05-30 21:54:31 -0500

    prints list with occurences

--- wordcount.py
+++ wordcount.py
@@ -2,5 +2,22 @@
 
 
 # put your code here.
+
+
 def get_word_count(file_name):
-        '''Open a file and prints out how many times each word shows up in the file'''
+    '''Open a file and prints out how many times each word shows up in the file'''
+    
+    text_for_counting = open(file_name)
+
+    word_count_dict = {}
+
+    for line in text_for_counting:
+        line = line.rstrip()
+        words = line.split(" ")
+
+        for word in words:
+            word_count_dict[word] = word_count_dict.get(word, 0) + 1
+
+    return word_count_dict
+
+print(get_word_count("test.txt"))
